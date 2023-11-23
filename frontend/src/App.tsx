ef0cef04333506259911be24ce1c7802b28ac0aa
JunSeong <gentlmanjun@naver.com>
2023-11-23 18:35:10 +0900

    [FRONT] feat | routing main page_LOST42-23

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,9 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          Hello World!
-      </header>
-    </div>
+    <Outlet/>
   );
 }
 
