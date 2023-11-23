a1d3ef94ebbfc523caf42aa3b6827931182be0ce
JunSeong <gentlmanjun@naver.com>
2023-11-23 21:55:34 +0900

    [FRONT] modify | change axios to fetch_LOST42-23

--- frontend/src/api/api.tsx
+++ frontend/src/api/api.tsx
@@ -2,12 +2,20 @@ import axios from "axios";
 import { api } from "./constant";
 
 export const oauth = async () => {
-  try {
-    const response = await axios.get(api.testLogin());
-    console.log(response);
-    return response.data;
-  } catch (error) {
-    console.log(error);
-    return null;
+  // try {
+    // const response = await axios.get(api.testLogin());
+  //   console.log(response);
+  //   return response.data;
+  // } catch (error) {
+  //   console.log(error);
+  //   return null;
+  // }
+  let response = await fetch(api.testLogin());
+  if(response.ok) {
+    let json = await response.json();
+    console.log(json);
+    return json;
+  } else {
+    console.log("HTTP-Error: " + response.status);
   }
 }
