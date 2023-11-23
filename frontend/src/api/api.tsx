ff79594b63c0210c929e68ce274a84bcfb7b0bd8
JunSeong <gentlmanjun@naver.com>
2023-11-23 19:05:09 +0900

    [FRONT] modify | add console.log to show response_LOST42-23

--- frontend/src/api/api.tsx
+++ frontend/src/api/api.tsx
@@ -4,9 +4,10 @@ import { api } from "./constant";
 export const oauth = async () => {
   try {
     const response = await axios.get(api.testLogin());
+    console.log(response);
     return response.data;
   } catch (error) {
-    console.error(error);
+    console.log(error);
     return null;
   }
 }
