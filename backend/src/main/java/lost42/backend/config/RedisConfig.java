cd006e2739c42c441bb3868f97078d925f751502
dnjsals45 <dnjsals45@naver.com>
2023-12-01 14:12:37 +0900

    [BACKEND] config | configuration of Redis Server_LOST42-34

---
+++ backend/src/main/java/lost42/backend/config/RedisConfig.java
@@ -0,0 +1,28 @@
+package lost42.backend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+    @Value("${spring.cache.redis.host}")
+    private String host;
+
+    @Value("${spring.cache.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(host, port);
+
+        LettuceClientConfiguration clientConfiguration = LettuceClientConfiguration.builder().build();
+        return new LettuceConnectionFactory(redisConfig, clientConfiguration);
+    }
+}
