602b6aefce06cefd3b544d0ed82dadc42c63defe
dnjsals45 <dnjsals45@naver.com>
2024-01-03 21:02:22 +0900

    [BACKEND] feat | change Authentication valid with custom annotation_LOST42-39

---
+++ backend/src/main/java/lost42/backend/common/jwt/JwtTokenValidation.java
@@ -0,0 +1,12 @@
+package lost42.backend.common.jwt;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface JwtTokenValidation {
+    boolean enable() default true;
+}
---
+++ backend/src/main/java/lost42/backend/common/jwt/interceptor/JwtTokenValidationInterceptor.java
@@ -0,0 +1,76 @@
+package lost42.backend.common.jwt.interceptor;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lost42.backend.common.auth.dto.CustomUserDetails;
+import lost42.backend.common.auth.exception.AuthErrorCode;
+import lost42.backend.common.auth.service.CustomUserDetailsService;
+import lost42.backend.common.jwt.JwtTokenUtil;
+import lost42.backend.common.jwt.JwtTokenValidation;
+import lost42.backend.common.response.ErrorResponse;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class JwtTokenValidationInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod handlerMethod = (HandlerMethod) handler;
+
+            JwtTokenValidation annotation = AnnotationUtils.findAnnotation(handlerMethod.getMethod(), JwtTokenValidation.class);
+            if (annotation != null && annotation.enable()) {
+                log.warn("JwtInterceptor: {}", request.getRequestURI());
+                String accessToken = resolveAccessToken(request);
+                String refreshToken = resolveRefreshToken(request);
+
+                if (accessToken == null) {
+                    handleAuthErrorException(response, AuthErrorCode.EMPTY_ACCESS_TOKEN);
+                    return false;
+                } else if (refreshToken == null) {
+                    handleAuthErrorException(response, AuthErrorCode.EMPTY_REFRESH_TOKEN);
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public String resolveAccessToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    public String resolveRefreshToken(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("refreshToken".equals(cookie.getName()))
+                    return cookie.getValue();
+            }
+        }
+        return null;
+    }
+
+    private void handleAuthErrorException(HttpServletResponse response, AuthErrorCode errorCode) throws IOException {
+        response.setStatus(errorCode.getHttpStatus().value());
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        String json = new ObjectMapper().writeValueAsString(ErrorResponse.of(errorCode.getMessage()));
+        response.getWriter().write(json);
+    }
+}
+
